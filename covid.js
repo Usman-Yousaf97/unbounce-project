9778dcc478769c3244a33184e695b014971a574b
usman yousaf <usmanusman97@gmail.com>
2023-04-06 02:21:46 -0700

    save

---
+++ covid.js
@@ -0,0 +1,28 @@
+
+
+// active nav
+
+
+
+let nav =  document.querySelector(".navigation-wrap");
+window.onscroll= function(){
+    if(document.documentElement.scrollTop > 60){
+        nav. classList.add("scroll-on")
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+
+// nav hide
+
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar = forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+
